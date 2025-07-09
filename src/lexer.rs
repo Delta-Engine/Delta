c3bbec96b05594e086daa5a10d5f701c30976d1a
Pranav Verma <pranav@verma.net.in>
2025-07-09 22:41:19 +0530

    Update: Implement Lexer struct with initialization and character advancement methods

--- src/lexer.rs
+++ src/lexer.rs
@@ -42,3 +42,30 @@ pub struct Lexer<'a> {
     column: usize,
     indent_stack: Vec<usize>,
 }
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let mut lexer = Lexer {
+            input,
+            position: 0,
+            current_char: None,
+            line: 1,
+            column: 0,
+            indent_stack: vec![0],
+        }; 
+        lexer.current_char = lexer.input.chars().next();
+        lexer
+    }
+
+    fn advance(mut self) {
+        if self.current_char == Some('\n') {
+            self.line = self.line + 1;
+            self.column = 0;
+        } else {
+            self.column = self.column + 1;
+        }
+
+        self.position = self.position + 1;
+        self.current_char = self.input.chars().nth(self.position);
+    }   
+}
