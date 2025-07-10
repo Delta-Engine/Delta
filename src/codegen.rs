fc5c24164fd6ecc346c027a46edf50e37479ac6e
Pranav Verma <pranav@verma.net.in>
2025-07-10 23:30:31 +0530

    Update: Implement statement interpretation and expression evaluation in CodeGenerator

--- src/codegen.rs
+++ src/codegen.rs
@@ -24,4 +24,54 @@ impl CodeGenerator {
         }
     }
 
+    fn interpret_statement(&mut self, statement: &Statement) -> Result<(), String> {
+        match statement {
+            Statement::Show(show) => {
+                let value = self.evaluate_expression(&show.value)?;
+                println!("{}", value);
+            }
+            Statement::Let(_) => {
+                // TODO: Add Let Statement Match
+                // For now, we wil acknowledge the let statement.
+                println!("Let statement (not yet implemented in interpreter)");
+            }
+            Statement::When(_) => {
+                // TODO: Add When Statement Match
+                // For now, we wil acknowledge the when statement.
+                println!("When statement (not yet implemented in interpreter)");
+            }
+            Statement::FunctionDef(_) => {
+                // TODO: Add Function Statement Match
+                // For now, we wil acknowledge the function definition statement.
+                println!("Function definition (not yet implemented in interpreter)");
+            }
+            Statement::Expression(expr) => {
+                let _value = self.evaluate_expression(expr)?;
+                // Expression statements don't print by default
+            }
+        }
+        Ok(())
+    }
+
+    fn evaluate_expression(&mut self, expression: &Expression) -> Result<String, String> {
+        match expression {
+            Expression::Number(n) => Ok(n.to_string()),
+            Expression::String(s) => Ok(s.clone()),
+            Expression::Identifier(name) => {
+                // TODO: Add Identifier Match.
+                // For now, les just return the identifier name.
+
+                Ok(format!("<identifier: {}>", name))
+            }
+            Expression::BinaryOp(binop) => {
+                let left = self.evaluate_expression(&binop.left)?;
+                let right = self.evaluate_expression(&binop.right)?;
+                Ok(format!("({} {:?} {})", left, binop.operator, right))
+            }
+            Expression::FunctionCall(call) => {
+                Ok(format!("<function call: {}>", call.name))
+            }
+        }
+    }
+
 }
